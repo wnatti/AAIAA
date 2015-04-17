9cc79cdc086f008e4f21e19007312756150e2bdd
tti13santtie <TTI13SAnttiE@kamk.fi>
2015-04-17 11:48:26 +0300

    main spritepiirtoon

--- Siika2D/Siika2D/jni/main.cpp
+++ Siika2D/Siika2D/jni/main.cpp
@@ -1,56 +1,86 @@
-#include "../engine/core/Siika2D.h"
+
+#include "..\engine\core\Siika2D.h"
 #include "../engine/graphics/BufferManager.h"
 #include "../engine/graphics/ShaderManager.h"
-
-bool doneOnce = false;
+#include "../engine/core/ResourceManager.h"
+#include "../engine/graphics/SpriteManager.h"
+#include "../engine/graphics/TextureManager.h"
+#include "../engine/core/MemoryManager.h"
+#include "../engine/audio/Audio.h"
 
 core::Siika2D *siika = core::Siika2D::UI();
 
+graphics::ShaderManager *shaderManager;
+graphics::BufferManager *buffManager;
+graphics::SpriteManager *spriteManager;
+core::ResourceManager *resourceManager;
+graphics::TextureManager * texMngr;
 
+bool managersDone = false;
+graphics::Sprite * sprite, *sprite2;
 GLfloat vertices[] =
 {
-	-1.0, -1.0, 1.0, 1.0, 1.0, 1.0,
-	0.0, 1.0, 1.0, 1.0, 1.0, 1.0,
-	1.0, -1.0, 1.0, 1.0, 1.0, 1.0
+	-1.0, -1.0, 0.0, 0.0, 1.0, 1.0,
+	0.0, 1.0, 0.0, 0.0, 1.0, 1.0,
+	1.0, -1.0, 0.0, 0.0, 1.0, 1.0,
+	1.0, -1.0, 0.0, 0.0, 1.0, 1.0
 };
 
 GLint indices[] =
 {
-	1, 2, 3
+	//1, 2, 3
+	1, 2, 4, 2, 3, 4
 };
-
+float pos = 0;
+uint blue = 1;
+uint green = 128;
 void doStuff()
 {
 	
-	if (!doneOnce)
+	if(!managersDone)
 	{
-		siika->GRAPHICS->useShader();
-		siika->GRAPHICS->setAttributes(graphics::shdrAtrib::position, graphics::shdrAtrib::color, graphics::shdrAtrib::unknown);
+		siika->GRAPHICS->setAttributes(graphics::shdrAtrib::position, graphics::shdrAtrib::color, graphics::shdrAtrib::texture);
+		siika->GRAPHICS->useShader(true, true);
 
-		siika->GRAPHICS->addVertices(vertices, sizeof(vertices));
-		siika->GRAPHICS->addIndices(indices, sizeof(indices));
+		resourceManager = &siika->_resourceManager;
+		spriteManager = new graphics::SpriteManager(siika->GRAPHICS, siika->GRAPHICS);
+		texMngr = new graphics::TextureManager(resourceManager);
+		graphics::Texture * tex = texMngr->createTexture("tekstuuri.png");
+		sprite = spriteManager->createSprite(glm::vec2(0, 0), glm::vec2(0.5, 0.5), glm::vec2(0.2, 0.2), tex, glm::vec2(0, 1), glm::vec2(1, 0));
+		
+		siika->GRAPHICS->useDefaultShader(false, true);
+		sprite2 = spriteManager->createSprite(glm::vec2(0.5, 0.5), glm::vec2(0.5, 0.5), glm::vec2(0.2, 0.2), tex, glm::vec2(1, 0), glm::vec2(0, 1));
+
+		managersDone = true;
+		glActiveTexture(GL_TEXTURE0);
+		//Audio ad;
+	}
+
+
+
 
 	
-		doneOnce = !doneOnce;
-
-		siika->INPUT->enableAccelerometer();
-	}
-
-	int i = 0;
-	while (i < siika->INPUT->_accelerometerData.size())
-	{
-		s2d_info("%f %f %f", siika->INPUT->_accelerometerData[i].x, siika->INPUT->_accelerometerData[i].y, siika->INPUT->_accelerometerData[i].z);
-		i++;
-	}
-
+	green += 2;
+	sprite->setColor(graphics::Color(0, green, blue++, 255));
+	if(blue > 254)
+		blue = 0;
+	if(green > 252)
+		green = 0;
+	pos = pos + 0.01;
+	if(pos > 1.0f)
+		pos = -1.0;
+	sprite->setPosition(glm::vec2(pos, sprite->getPosition()->y));
 	siika->GRAPHICS->clear();
-	siika->GRAPHICS->draw();
+	//siika->GRAPHICS->draw();
+	spriteManager->drawSprites();
+//	siika->GRAPHICS->draw();
 	siika->GRAPHICS->swap();
+
+
 }
 
 void siika_main()
 {
-
 	doStuff();
 
 }
