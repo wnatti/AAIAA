486dab2bbe0c6f747458adedd05926f7d1f8dc67
nolongerme <iiro.heikkinen@hotmail.com>
2015-05-07 15:33:13 +0300

    AudioManager
    AudioManager lisätty. Sen kautta luodaan audio objeektit.

---
+++ Siika2D/Siika2D/engine/audio/AudioManager.cpp
@@ -0,0 +1,29 @@
+#include "AudioManager.h"
+
+using namespace audio;
+
+AudioManager::AudioManager(core::ResourceManager* resourceManager)
+{
+	_resourceManager = resourceManager;
+}
+
+
+AudioManager::~AudioManager()
+{
+
+}
+
+Audio* AudioManager::createAudio(std::string filename)
+{
+	std::map<std::string, Audio>::iterator it = _createdAudio.find(filename);
+	if (it != _createdAudio.end())
+		return &it->second;
+	else
+	{
+		Audio audio(filename, _resourceManager);
+
+		_createdAudio.insert(std::make_pair(filename, audio));
+
+		return &_createdAudio.at(filename);
+	}
+}
---
+++ Siika2D/Siika2D/engine/audio/AudioManager.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "Audio.h"
+
+namespace audio
+{
+	/**
+	Audio objects are created in AudioManager.
+	Keeps track of created audio objects.
+	*/
+	class AudioManager
+	{
+	public:
+		/**
+		Constructor.
+		Takes a reference to ResourceMananager as a parameter.
+		*/
+		AudioManager(core::ResourceManager* resourceManager);
+		~AudioManager();
+
+		/**
+		Creates new audio object from .ogg file or returns an existing one.
+		Audio is loaded with ResourceManager.
+		*/
+		Audio* createAudio(std::string filename);
+	private:
+		core::ResourceManager* _resourceManager;
+		std::map<std::string, Audio> _createdAudio;
+
+	};
+}
